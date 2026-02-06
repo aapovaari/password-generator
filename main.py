6eca6c5ffcea08ac91b733405d11ef48e17990f0
Aapo Saari <aapo.juh.saari@gmail.com>
2026-02-06 10:37:17 +0200

    Kirjoitusvirheen korjaus + modulaarisuuden lisäys

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 import secrets
 import string
 
-print('Anna salasanan pituus.')
-print('Salasanan vähimmäispituus on 4 merkkiä (suositus vähintään 12 merkkiä)')
+def main():
+    print('Anna salasanan pituus.')
+    print('Salasanan vähimmäispituus on 4 merkkiä (suositus vähintään 12 merkkiä)')
 
-while True:
-    try:
-        pw_lenght = int(input('Salasanan pituus--> '))
-        if pw_lenght < 4:
-            print('Virhe: Salasanan vähimmäispituus on 4 merkkiä')
-        else:
-            break
-    except ValueError:
-        print('Virhe: Syötä kokonaisluku.')
+    while True:
+        try:
+            pw_length = int(input('Salasanan pituus--> '))
+            if pw_length < 4:
+                print('Virhe: Salasanan vähimmäispituus on 4 merkkiä')
+            else:
+                break
+        except ValueError:
+            print('Virhe: Syötä kokonaisluku.')
 
 
-lowercases = string.ascii_lowercase
-uppercases = string.ascii_uppercase
-digits = string.digits
-special_characters = string.punctuation
+    lowercases = string.ascii_lowercase
+    uppercases = string.ascii_uppercase
+    digits = string.digits
+    special_characters = string.punctuation
 
-all_characters = lowercases + uppercases + digits + special_characters
+    all_characters = lowercases + uppercases + digits + special_characters
 
-chosen_lc = secrets.choice(lowercases)
-chosen_uc = secrets.choice(uppercases)
-chosen_d = secrets.choice(digits)
-chosen_sc = secrets.choice(special_characters)
+    chosen_lc = secrets.choice(lowercases)
+    chosen_uc = secrets.choice(uppercases)
+    chosen_d = secrets.choice(digits)
+    chosen_sc = secrets.choice(special_characters)
 
-password_list = [chosen_lc, chosen_uc, chosen_d, chosen_sc]
+    password_list = [chosen_lc, chosen_uc, chosen_d, chosen_sc]
 
-for i in range(pw_lenght - 4):
-    new_character = secrets.choice(all_characters)
-    password_list.append(new_character)
+    for i in range(pw_length - 4):
+        new_character = secrets.choice(all_characters)
+        password_list.append(new_character)
 
-secrets.SystemRandom().shuffle(password_list)
-password = ''.join(password_list)
+    secrets.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
 
-print(f'Uusi salasanasi on: {password}')
+    print(f'Uusi salasanasi on: {password}')
+
+if __name__ == "__main__":
+    main()
